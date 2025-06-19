1c0f48b559b53097b159222ba4185907efc68313
finn79426 <finn79426@gmail.com>
2025-06-19 15:18:03 +0800

    feat: add constant

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import os
+
+# ------------------------------------------------------------------
+#                             CONSTANT
+# ------------------------------------------------------------------
+PROJECT_ROOT_ABS_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+THIS_DIR_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+THIS_FILE_BASE_NAME = os.path.splitext(os.path.basename(__file__))[0]
+# ------------------------------------------------------------------
+
 def greeting(name: str) -> str:
+    print(f'{PROJECT_ROOT_ABS_PATH=}')
+    print(f'{THIS_DIR_ABS_PATH=}')
+    print(f'{THIS_FILE_BASE_NAME=}')
     return f"Hello {name}"
